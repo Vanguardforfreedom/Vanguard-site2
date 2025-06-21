676753bf9afc62216c0a8b52de7fe95a3175c77e
Vanguardforfreedom <vanguardforfreedom@gmail.com>
2025-06-21 14:30:01 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.fade-in, .slide-in').forEach(el => {
-    el.style.opacity = '1';
-    el.style.transform = 'translateY(0)';
-  });
-});
